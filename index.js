1a07690310e02a99cd7ba6945f3367223aeea2ad
Philipp-Hess <145116105+Philipp-Hess@users.noreply.github.com>
2023-09-27 16:46:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+console.clear();
+
+// Part 1: Password
+const SUPER_SECRET_PASSWORD = "h4x0r1337";
+
+const receivedPassword = "password1234";
+
+if (SUPER_SECRET_PASSWORD === receivedPassword) {
+  console.log(`Welcome! You are logged in as Brunhilde1984.`);
+} else {
+  console.log(`Access denied!`);
+}
+
+// Part 2: Even / Odd
+const number = 12;
+
+if (number % 2 == 0) {
+  console.log(`even number`);
+} else {
+  console.log(`odd number`);
+}
+
+// Part 3: Hotdogs
+const numberOfHotdogs = 42;
+
+if ((numberOfHotdogs) => 5) {
+  console.log(`2 euro per hotdog`);
+} else {
+  console.log;
+}
+
+// Part 4: Daytime
+const currentHour = 12;
+
+const statement = "";
+
+console.log(statement);
+
+// Part 5: Greeting
+const name = "Archibald";
+
+const greeting = "Hello " + "//enter your code here" + "!";
+
+console.log(greeting);
